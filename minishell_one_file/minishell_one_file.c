cf5a96585cb387d840734e494887d22129201927
ablaamim <aku-no-hana@outlook.fr>
2022-06-05 00:48:09 +0100

    lexer and parser gone prrrrrrrrrrrrrrrrrr

--- minishell_one_file/minishell_one_file.c
+++ minishell_one_file/minishell_one_file.c
@@ -6,7 +6,7 @@
 /*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/04 22:21:52 by ablaamim          #+#    #+#             */
-/*   Updated: 2022/06/04 23:11:22 by ablaamim         ###   ########.fr       */
+/*   Updated: 2022/06/05 00:47:11 by root             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,80 @@
 #include <readline/history.h>
 #include <stdbool.h>
 
-#define ERROR_MINISHELL "error : exit minishell\n"
+#define ERROR_MINISHELL "Error : exit minishell\n"
+#define WARNING "NOT INLINE MODE\n!!!!!!"
+
+/*
+ * All code types of nodes that can be found in out AST,
+ * leaves are considered as nodes
+*/
+
+enum e_node_type
+{
+	SIMPLE_CMD,
+	PIPE_NODE,
+	OR_NODE,
+	AND_NODE,
+	SEMICO_NODE
+};
+
+/*
+ * All the possible types of redirections that can be found in a cmd
+*/
+
+enum e_redirection_type
+{
+	INPUT_REDIR,
+	HEREDOC_REDIR,
+	OUTPUT_REDIR,
+	APPEND_OUTPUT_REDIR,
+	NO_REDIR
+};
+
+/*
+ * Simple cmd
+*/
+
+typedef struct s_simple_cmd
+{
+	char	**argv;
+	int		fd_in;
+	int		fd_out;
+	bool	input_stream_has_quotes;
+}	t_simple_cmd;
+
+/*
+ * A node in our AST always has children, one on the left, and one one
+ * the right
+*/
+
+typedef struct s_child
+{
+	struct s_node	*left;
+	struct s_node	*right;
+}	t_child;
+
+/*
+ * If the member of the AST is a leaf, it contains a simple command,
+ * otherwise if it is a node, is had two childs !
+*/
+
+typedef union u_node_content
+{
+	struct s_simple_cmd	simple_cmd;
+	struct s_child		child;
+}	t_node_content;
+
+/*
+ * A member of the AST contains a node_type and a content which can be a
+ * simple command or a child
+*/
+
+typedef	struct s_node
+{
+	enum e_node_type	type;
+	t_node_content		content;
+}	t_node;
 
 /*
  * Function to print content of env
@@ -49,7 +122,34 @@ char	*read_line(bool inline_mode)
 		line = readline("minishell$>");
 		if (!line)
 			return (NULL);
+		//add_history(line);
+	}
+	else
+		write(2, WARNING, sizeof(WARNING));
+	return (line);
+}
+
+/*
+ * Lexer / Parser
+*/
+
+t_node	*lexer_parser(char *line)
+{
+	t_node	*ast;
+}
+
+/*
+ * Save history, parse commands and executes them
+*/
+
+void	execution(char *line, bool inline_mode)
+{
+	t_node	*ast;
+	//printf("line = %s\ninline_mode = %d\n", line, inline_mode);
+	if (line != NULL)
+	{
 		add_history(line);
+		ast = lexer_parser(line);
 	}
 }
 
@@ -65,6 +165,7 @@ void	minishell(bool inline_mode)
 	while (true)
 	{
 		line = read_line(inline_mode);
+		execution(line, inline_mode);
 	}
 }
 
