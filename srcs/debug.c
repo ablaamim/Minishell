04d4de0941365ec8f02bdf5657db3c221455c009
ablaamim <aku-no-hana@outlook.fr>
2022-06-08 18:02:14 +0100

    bcs why not

--- srcs/debug.c
+++ srcs/debug.c
@@ -6,33 +6,16 @@
 /*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 11:06:55 by ablaamim          #+#    #+#             */
-/*   Updated: 2022/06/07 17:35:31 by ablaamim         ###   ########.fr       */
+/*   Updated: 2022/06/08 17:57:24 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	ft_display_ast(t_node *ast, int level)
-{
-	int	i;
 
-	if (ast == 0x0)
-		return ;
-	i = 0x0;
-	while (i < level)
-	{
-		printf("\t\t");
-		i++;
-	}
-	if (ast->type == SIMPLE_CMD)
-		printf("SIMPLE_CMD\n");
-	else if (ast->type == PIPE_NODE)
-		printf("PIPE_NODE\n");
-	else
-		printf("UNKNOWN COMMAND\n");
-	ft_display_ast(ast->content.child.left, level + 1);
-	ft_display_ast(ast->content.child.right, level + 1);
-}
+/*
+ * Function to print all of the env variables
+*/
 
 void	ft_print_env(char **env)
 {
