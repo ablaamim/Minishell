019e05eb07c01247eb57ff19427ebc334a61e0ab
Sh4dow <55363164+ablaamim@users.noreply.github.com>
2022-06-15 18:07:45 +0100

    Update minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -283,7 +283,8 @@ char				*ft_strstr(const char *big, const char *little);
 char				*ft_strchr(const char *s, int c);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 char				**ft_split(const char *s, char c);
-char				*ft_strjoin(char const *s1, char const *s2);
+char				*ft_strjoin(char const *s1, char const *s2, char const *sep);
+
 /*
  * Pipe streams define
 */
