598796a915fcb14291dc7f939eb7714663274527
Sh4dow <55363164+ablaamim@users.noreply.github.com>
2022-06-15 17:47:03 +0100

    Update ft_split_strjoin.c

--- srcs/ft_split_strjoin.c
+++ srcs/ft_split_strjoin.c
@@ -12,4 +12,106 @@
 
 #include "../includes/minishell.h"
 
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+  char	*joined;
+	size_t	i;
+	size_t	j;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	joined = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (joined == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[j] != '\0')
+	{
+		joined[i++] = s1[j];
+		j++;
+	}
+	j = 0;
+	while (s2[j] != '\0')
+	{
+		joined[i++] = s2[j];
+		j++;
+	}
+	joined[i] = '\0';
+	return (joined);
+}
+
+static int	ft_count_words(char const *s, char c)
+{
+	int	i;
+	int	count;
+
+	i = 0;
+	count = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			i++;
+		else
+		{
+			count++;
+			while (s[i] && s[i] != c)
+				i++;
+		}
+	}
+	return (count);
+}
+
+static char	*ft_make_words(char *word, char const *s, int j, int word_ln)
+{
+	int	i;
+
+	i = 0;
+	while (word_ln > 0)
+		word[i++] = s[j - word_ln--];
+	word[i] = '\0';
+	return (word);
+}
+
+static char	**ft_split_words(char **res, char const *s, char c, int word_ct)
+{
+	int	i;
+	int	j;
+	int	word_ln;
+
+	i = 0;
+	j = 0 ;
+	word_ln = 0;
+	while (s[j] && i < word_ct)
+	{
+		while (s[j] && s[j] == c)
+			j++;
+		while (s[j] && s[j] != c)
+		{
+			j++;
+			word_ln++;
+		}
+		res[i] = (char *)malloc(sizeof(char) * (word_ln + 1));
+		if (!res[i])
+			return (NULL);
+		ft_make_words (res[i], s, j, word_ln);
+		word_ln = 0;
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	int		word_ct;
+	char	**res;
+
+	if (s == NULL)
+		return (NULL);
+	word_ct = ft_count_words(s, c);
+	res = (char **)malloc(sizeof(char *) * (word_ct + 1));
+	if (!res)
+		return (NULL);
+	ft_split_words (res, s, c, word_ct);
+	return (res);
+}
