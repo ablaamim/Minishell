a7621ad7fcdb3e3804f7bcc0b8f4599cd93175d6
Sh4dow <55363164+ablaamim@users.noreply.github.com>
2022-06-15 18:14:26 +0100

    Create copy_concatenate_utils.c

---
+++ srcs/copy_concatenate_utils.c
@@ -0,0 +1,28 @@
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	j = 0;
+	i = ft_strlen(dest);
+	while (src[j])
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+}
