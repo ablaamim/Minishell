5a1b30ab877e54afb5855a9bb34037fdd869e814
ablaamim <aku-no-hana@outlook.fr>
2022-06-25 16:58:37 +0100

    Will comeback later to compelete this

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -6,7 +6,7 @@
 /*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 10:10:38 by ablaamim          #+#    #+#             */
-/*   Updated: 2022/06/25 01:29:04 by ablaamim         ###   ########.fr       */
+/*   Updated: 2022/06/25 01:50:16 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,37 +44,12 @@ int	main(int argc, char **argv, char **env)
 	if (argc == 1)
 	{
 		init_bash_env(file_extract(argv[0]), env);
-		/*
-		 * TO DO :
-		 * Init terminal, block display of ctrl+c, ctrl+d ... (^C, ^D ...)
-		*/
-		//terminal_initialization(0x0);
-		//ignoring_signals(SIGQUIT);
-		//ignoring_signals(SIGTSTP);
-		//ignoring_signals(SIGTTIN);
-		//ignoring_signals(SIGTTOU);
-		//ignoring_signals(SIGTERM);
-		//ignoring_signals(SIGHUP);
-		/*
-		 * Init bash environment.
-		*/
-		printf("============================================================\n\n");
-		//ft_print_env(env);
-		/*
-		 * Free all fd beyond or equal to 3.
-		*/
+		//printf("============================================================\n\n");
 		ft_free_fd();
-		/*
-		 * Core funtion.
-		*/
 		ft_minishell();
 	}
 	else
 	{
-		/*
-		 * print error using perror() on stderr in case if user enter extra
-		 * args.
-		*/
 		if (argv_error_handler(argv[1]))
 			return (*retrieve_exit_status());
 	}
