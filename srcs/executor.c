4d445814d255e11f2bb0047e163f6529ac0f7b63
mbistami <gruz@DESKTOP-P0G8G27.localdomain>
2022-08-20 00:45:48 +0100

    builtin

--- srcs/executor.c
+++ srcs/executor.c
@@ -34,10 +34,10 @@
  *
  */
 
-int	ft_is_built_in(char *string)
+int ft_is_built_in(char *string)
 {
-	int		i;
-	char	**built_ins;
+	int i;
+	char **built_ins;
 
 	i = 0;
 	built_ins = ft_split("env pwd echo", ' ');
@@ -54,7 +54,7 @@ int	ft_is_built_in(char *string)
 	return (0);
 }
 
-int	ft_argv_len(char **argv)
+int ft_argv_len(char **argv)
 {
 	int i;
 
@@ -64,33 +64,71 @@ int	ft_argv_len(char **argv)
 	return (i);
 }
 
+void ft_echo_iterator(char **args, int *k, int i)
+{
+	*k = 1;
+	while (args[i][*k] == 'n')
+		(*k)++;
+}
+
+void ft_echo_print(char **args, int i, int j, int add_new_line)
+{
+	while (args[i])
+	{
+		printf("%s", args[i++]);
+		if (i < j)
+			printf(" ");
+	}
+	if (add_new_line)
+		printf("\n");
+}
+
+void ft_handle_echo(char **args)
+{
+	int i;
+	int j;
+	int add_new_line = 1;
+	int k;
+
+	i = 1;
+	j = ft_argv_len(args);
+	while (args[i] && args[i][0] == '-' && args[i][1] == 'n')
+	{
+		ft_echo_iterator(args, &k, i);
+		if ((size_t)k < ft_strlen(args[i]))
+			break;
+		add_new_line = 0;
+		i++;
+	}
+	ft_echo_print(args, i, j, add_new_line);
+}
+
 /*
  * Built-in manager.
  * built_in env works like a chrm now.
-*/
+ */
 
 void ft_handle_built_ins(char **args, char **env, int *error)
 {
-	//env = env;
-	//args = args;
 	if (!ft_strcmp(args[0], "env"))
 	{
 		if (ft_argv_len(args) > 1)
 		{
-			//write(2, "ERROR", 5);
 			variadic_error_printer(2, "env : %s %s", args[1], ENV_ERROR);
 			*error = 2;
 		}
 		else
 			ft_print_env(env);
 	}
+	else if (!ft_strcmp(args[0], "echo"))
+		ft_handle_echo(args);
 }
 
 void ft_handle_cmd(t_node *node, int htf, char **env, int *error)
 {
-	int		pid;
-	char	*bin_path;
-	char	**argv;
+	int pid;
+	char *bin_path;
+	char **argv;
 
 	//(void) *error;
 	//(void) htf;
@@ -98,7 +136,7 @@ void ft_handle_cmd(t_node *node, int htf, char **env, int *error)
 	bin_path = found_binary(argv);
 	if (htf == 1)
 	{
-		//execve(node->content.simple_cmd.argv[0], node->content.simple_cmd.argv, env);
+		// execve(node->content.simple_cmd.argv[0], node->content.simple_cmd.argv, env);
 		execve(bin_path, argv, env);
 	}
 	pid = fork();
@@ -116,7 +154,7 @@ int ft_exec_cmd(t_node *node, int htf, char **env)
 	int error;
 
 	error = 0;
-	//printf("ARGS\n");
+	// printf("ARGS\n");
 	if (ft_is_built_in(node->content.simple_cmd.argv[0]))
 		ft_handle_built_ins(node->content.simple_cmd.argv, env, &error);
 	else
@@ -180,15 +218,15 @@ void ft_iterate_tree(t_node *node, int has_to_fork, int exec_index, char **env)
 	{
 		/*
 		 * Labghiti tkhdemhum recursively hahya function w data raha organized in simple_cmd
-		*/
+		 */
 		/*if (execute_redirections(node) == true) // See exec_redirections.c
 		{
 		*/
-			if (node->type == PIPE_NODE)
-				ft_handle_pipe(node, exec_index, env);
-			else if (node->type == SIMPLE_CMD)
-				ft_exec_cmd(node, has_to_fork, env);
-		
+		if (node->type == PIPE_NODE)
+			ft_handle_pipe(node, exec_index, env);
+		else if (node->type == SIMPLE_CMD)
+			ft_exec_cmd(node, has_to_fork, env);
+
 		/*
 		}
 		else
@@ -201,28 +239,28 @@ void ft_iterate_tree(t_node *node, int has_to_fork, int exec_index, char **env)
 
 /*
  * -> Cache env is ready now !
-*/
+ */
 
 void ft_executor(char *line, char **env)
 {
-	t_node	*ast;
-	t_env	*bash_env;
+	t_node *ast;
+	t_env *bash_env;
 
 	ast = 0x0;
 	bash_env = get_bash_env();
-	(void) env;
-	//printf("======================= MINI HELL CACHE =======================\n");
-	//ft_print_env(*bash_env);
+	(void)env;
+	// printf("======================= MINI HELL CACHE =======================\n");
+	// ft_print_env(*bash_env);
 	if (line != 0x0)
 	{
 		ast = ft_lexer_parser_program(line);
 		{
 			if (ast != 0x0)
 			{
-				//env = env;
-				//printf("\n**%d\n", ast->type);
+				// env = env;
+				// printf("\n**%d\n", ast->type);
 				ft_iterate_tree(ast, 0, 0, *bash_env);
-				//printf("\n**%d\n", ast->type);
+				// printf("\n**%d\n", ast->type);
 
 				// execute_ast_data(ast);
 				// ast_clearing(&ast);
