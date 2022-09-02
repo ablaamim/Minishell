18b383b0680a33088c9cebc9d10fee081325c603
ablaamim <aku-no-hana@outlook.fr>
2022-09-03 00:40:56 +0100

    heredoc special case fixef

--- srcs/ft_lexer_parser_program.c
+++ srcs/ft_lexer_parser_program.c
@@ -6,7 +6,7 @@
 /*   By: ablaamim <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 16:40:32 by ablaamim          #+#    #+#             */
-/*   Updated: 2022/08/29 12:26:40 by ablaamim         ###   ########.fr       */
+/*   Updated: 2022/09/03 00:39:57 by ablaamim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,18 +25,6 @@
  * data in accessible memory.
  */
 
-/*
- * BUGS I FOUND till now : a pipe without output command.
- *
- * $> ls | ls |
- *
- * $> segmentation fault.
- *
- * [CASE CLOSED] : if disp_tree() which is just a debuger is commented, segv
- * is eliminated
- *
- */
-
 t_node	*ft_lexer_parser_program(char *line)
 {
 	t_node	*ast;
@@ -50,8 +38,11 @@ t_node	*ft_lexer_parser_program(char *line)
 				token_list != 0x0)
 		{
 			if (token_list != 0x0)
-				variadic_error_printer(2, "%s : %s\n", ERROR_TOKEN, \
-						token_list->data);
+			{
+				variadic_error_printer(2, "%s :", \
+				"Error : syntax error near unexpected token");
+				variadic_error_printer(2, "%s\n", token_list->data);
+			}
 			exit_value_set(258);
 			ast_clearing(&ast);
 		}
