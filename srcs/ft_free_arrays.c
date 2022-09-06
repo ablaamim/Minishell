5e316322632fb36239655ca65f0797a79046ad76
mbistami <gruz@DESKTOP-P0G8G27.localdomain>
2022-09-06 20:35:21 +0100

    norms

--- srcs/ft_free_arrays.c
+++ srcs/ft_free_arrays.c
@@ -6,7 +6,7 @@
 /*   By: gruz <gruz@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/15 18:21:49 by ablaamim          #+#    #+#             */
-/*   Updated: 2022/09/06 00:05:02 by ablaamim         ###   ########.fr       */
+/*   Updated: 2022/09/06 20:34:35 by gruz             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,13 @@ void	ft_free_arrays(char **arrays)
 	free(arrays);
 }
 
-void ft_handle_child_init(t_node *node, t_pipe **pipe, int exec_index)
+void	ft_handle_child_init(t_node *node, t_pipe **pipe, int exec_index)
 {
-	int **pipes;
+	int	**pipes;
 
-	if (node->content.simple_cmd.argv[0] && ft_is_child_ignored(node->content.simple_cmd.argv[0]) && ft_lstsize(*pipe) == 0)
+	if (node->content.simple_cmd.argv[0]
+		&& ft_is_child_ignored(node->content.simple_cmd.argv[0])
+		&& ft_lstsize(*pipe) == 0)
 		exit(0);
 	pipes = ft_to_array(pipe);
 	ft_handle_wildcard(node);
@@ -37,14 +39,29 @@ void ft_handle_child_init(t_node *node, t_pipe **pipe, int exec_index)
 	ft_free_to_array(pipe, pipes);
 }
 
-void ft_handle_child_execution(t_node *node)
+void	ft_handle_child_extanded(t_node *node,
+	char **bin_path, char **argv, int *ret)
 {
-	char *bin_path;
-	char **argv;
-	int ret;
-	t_env *env;
+	t_env	*env;
 
 	env = get_bash_env();
+	*bin_path = found_binary(argv);
+	*ret = manage_execution(bin_path, node->content.simple_cmd.argv[0]);
+	if (*ret != EXIT_SUCCESS)
+	{
+		exit_value_set(*ret);
+		exit(*ret);
+	}
+	if (execve(*bin_path, argv, *env) == ERR)
+		*ret = manage_execution(bin_path, node->content.simple_cmd.argv[0]);
+}
+
+void	ft_handle_child_execution(t_node *node)
+{
+	char	*bin_path;
+	char	**argv;
+	int		ret;
+
 	if (ft_is_built_in(node->content.simple_cmd.argv[0]))
 	{
 		ret = ft_handle_built_ins(node->content.simple_cmd.argv, node);
@@ -55,50 +72,53 @@ void ft_handle_child_execution(t_node *node)
 	argv = node->content.simple_cmd.argv;
 	if (!argv[0])
 		exit(1);
-	bin_path = found_binary(argv);
-	ret = manage_execution(&bin_path, node->content.simple_cmd.argv[0]);
-	if (ret != EXIT_SUCCESS)
-	{
-		exit_value_set(ret);
-		exit(ret);
-	}
-	if (execve(bin_path, argv, *env) == ERR)
-		ret = manage_execution(&bin_path, node->content.simple_cmd.argv[0]);
+	ft_handle_child_extanded(node, &bin_path, argv, &ret);
 	garbage_free((void **)&bin_path);
 	exit(ret);
 }
 
-void ft_handle_child(t_node *node, t_pipe **pipe, int exec_index)
+void	ft_handle_child(t_node *node, t_pipe **pipe, int exec_index)
 {
 	ft_handle_child_init(node, pipe, exec_index);
 	ft_handle_child_execution(node);
 }
 
-void ft_handle_parent(t_node *node, int pid, t_pipe **pipe)
+void	ft_handle_parent_extanded(int pid, int *status, int *gab)
 {
-	int status;
-	int gab;
+	while (waitpid(pid, status, 0x0) > 0)
+	{
+		while (waitpid(-1, gab, 0x0) > 0)
+			;
+		if (WIFEXITED(*status))
+		{
+			exit_value_set(WEXITSTATUS(*status));
+			if (WEXITSTATUS(*status) == 17)
+				exit_value_set(0x0);
+		}
+		if (WIFSIGNALED(*status))
+			exit_value_set(WTERMSIG(*status) + 128);
+	}
+}
+
+void	ft_handle_parent(t_node *node, int pid, t_pipe **pipe)
+{
+	int	status;
+	int	gab;
 
 	signal(SIGINT, SIG_IGN);
 	if (pid)
-	while (waitpid(pid, &status, 0x0) > 0)
 	{
-		while (waitpid(-1, &gab, 0x0) > 0)
-				;
-		if (WIFEXITED(status))
-		{
-			exit_value_set(WEXITSTATUS(status));
-			if (WEXITSTATUS(status) == 17)
-				exit_value_set(0x0);
-		}
-		if (WIFSIGNALED(status))
-			exit_value_set(WTERMSIG(status) + 128);
+		ft_handle_parent_extanded(pid, &status, &gab);
 	}
 	if (node->content.simple_cmd.argv[0] == 0x0)
 		return ;
 	status = *retrieve_exit_status();
-	if (ft_lstsize(*pipe) == 0 && (!ft_strcmp(node->content.simple_cmd.argv[0], "exit") || !ft_strcmp(node->content.simple_cmd.argv[0], "cd") || !ft_strcmp(node->content.simple_cmd.argv[0], "export") ||
-								   !ft_strcmp(node->content.simple_cmd.argv[0], "unset") || !ft_strcmp(node->content.simple_cmd.argv[0], "echo")))
+	if (ft_lstsize(*pipe) == 0
+		&& (!ft_strcmp(node->content.simple_cmd.argv[0], "exit")
+			|| !ft_strcmp(node->content.simple_cmd.argv[0], "cd")
+			|| !ft_strcmp(node->content.simple_cmd.argv[0], "export")
+			|| !ft_strcmp(node->content.simple_cmd.argv[0], "unset")
+			|| !ft_strcmp(node->content.simple_cmd.argv[0], "echo")))
 	{
 		status = ft_handle_built_ins(node->content.simple_cmd.argv, node);
 		exit_value_set(status);
